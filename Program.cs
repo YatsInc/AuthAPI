06ddaefd6440b96abb342742d5db158a91db5680
DennY <45600211+DennYats@users.noreply.github.com>
2021-11-18 21:04:41 +0200

    check if branch is protected

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddCors(options =>
                 .AllowAnyMethod();
         });
 });
+
 // Configure Identity to use the same JWT claims as OpenIddict instead
 // of the legacy WS-Federation claims it uses by default (ClaimTypes),
 // which saves you from doing the mapping in your authorization controller.
